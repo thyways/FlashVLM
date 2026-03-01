0edab80ba591e022738ba32ff4dde60e5da2a0c5
thyways <2484113689@qq.com>
2026-03-01 18:44:45 +0800

    init

--- llava/model/multimodal_resampler/qformer.py
+++ llava/model/multimodal_resampler/qformer.py
@@ -38,10 +38,15 @@ from transformers.modeling_outputs import (
 )
 from transformers.modeling_utils import (
     PreTrainedModel,
-    apply_chunking_to_forward,
-    find_pruneable_heads_and_indices,
-    prune_linear_layer,
+    # apply_chunking_to_forward,
+    # find_pruneable_heads_and_indices,
+    # prune_linear_layer,
 )
+from transformers.pytorch_utils import (
+    apply_chunking_to_forward, 
+    find_pruneable_heads_and_indices, 
+    prune_linear_layer
+ ) # change modeling_utils to pytorch_utils
 from transformers.utils import logging
 from transformers.models.bert.configuration_bert import BertConfig
 
