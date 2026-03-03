5bab8d605409e977513bdb3e36addb752c33937c
thyways <2484113689@qq.com>
2026-03-04 00:32:51 +0800

    add llava support

--- scripts/baseline/llava_vid.sh
+++ scripts/baseline/llava_vid.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export HF_HOME="${HOME}/.cache/huggingface"
-export HF_HUB_OFFLINE=1
 
-# Evaluation benchmarks. ("videomme" "egoschema" "mvbench" "longvideobench_val_v" "mlvu_test")
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+# Evaluation benchmarks.("videomme" "mvbench" "longvideobench_val_v" "egoschema")
 TASKS=("videomme")
 
 # Pretrained model path.
-PRETRAINED="llava-hf/LLaVA-NeXT-Video-7B-hf"
+PRETRAINED="lmms-lab/LLaVA-Video-7B-Qwen2"
 
-# Model arguments for llava_hf.
-MAX_FRAMES_NUM=32
+# Model arguments.
+MAX_FRAMES_NUM=64
+CONV_TEMPLATE=qwen_1_5
+FORCE_SAMPLE=True
+ADD_TIME_INSTRUCTION=False
+MM_SPATIAL_POOL_MODE=average # * Different from LLaVA-OneVision
+MM_NEWLINE_POSITION=frame # Add newline token after each frame.
 ATTN_IMPLEMENTATION=flash_attention_2
-BASE_MODEL_ARGS="pretrained=$PRETRAINED,max_frames_num=$MAX_FRAMES_NUM,attn_implementation=$ATTN_IMPLEMENTATION"
+BASE_MODEL_ARGS="pretrained=$PRETRAINED,conv_template=$CONV_TEMPLATE,mm_spatial_pool_mode=$MM_SPATIAL_POOL_MODE,mm_newline_position=$MM_NEWLINE_POSITION,max_frames_num=$MAX_FRAMES_NUM,attn_implementation=$ATTN_IMPLEMENTATION,force_sample=$FORCE_SAMPLE,add_time_instruction=$ADD_TIME_INSTRUCTION"
 
-MODEL_ARGS="$BASE_MODEL_ARGS"
+MODEL_ARGS="enable_flashvid=False,$BASE_MODEL_ARGS"
 for task in "${TASKS[@]}"; do
     echo "Evaluating task: $task"
     accelerate launch \
-    --main_process_port 12346 \
-    --num_processes 8 \
-    -m lmms_eval \
-    --model llava_hf \
-    --model_args "$MODEL_ARGS" \
-    --force_simple \
-    --tasks "$task" \
-    --batch_size 1 \
-    --log_samples \
-    --log_samples_suffix "llava_vid" \
-    --output_path ./logs/baseline
+        --main_process_port 18888 \
+        --num_processes 8 \
+        -m lmms_eval \
+        --model llava_vid \
+        --model_args $MODEL_ARGS \
+        --tasks $task \
+        --batch_size 1 \
+        --log_samples \
+        --log_samples_suffix "llava_vid" \
+        --output_path ./logs/baseline
 done
